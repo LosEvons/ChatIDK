1ecb3bbd5ea90a492041cbba3c23adfdd7a8e01c
Emil Malk <emil.malk@gmail.com>
2023-11-06 19:31:33 +0200

    Fixed some bugs and simplified.

--- chatidk/psql_db/cman.py
+++ chatidk/psql_db/cman.py
@@ -2,52 +2,43 @@ from typing import Optional
 from sqlalchemy.sql import text
 from werkzeug.security import check_password_hash, generate_password_hash
 
+from .user import User
+
 class CredentialManager:
     def __init__(self, p_db):
         self.__db = p_db
-        
-    def username_in_database(self, username) -> bool:
-        sql = text("SELECT id FROM users WHERE username=:username")
-        result = self.__db.session.execute(sql, {"username":username})
-        user = result.fetchone()
-        if not user:
-            return False
-        else:
-            return True
-        
-    def get_entry_id(self, username) -> Optional[int]:
-        sql = text("SELECT id FROM users WHERE username=:username")
+    
+    def __get_user(self, username) -> Optional[User]:
+        sql = text("SELECT id, username, password FROM users WHERE username=:username AND visible=TRUE")
         result = self.__db.session.execute(sql, {"username":username})
         user = result.fetchone()
         if not user:
             return None
         else:
-            return user.id
+            return User(user.id, user.username, user.password)
     
     def register(self, username, password) -> Optional[str]:
-        if self.username_in_database(username):
+        if self.__get_user(username):
             return "Username already in use!"
         else:
             hash_value = generate_password_hash(password)
-            sql = text("INSERT INTO users (username, password) VALUES (:username, :password)")
-            self.__db.session.execute(sql, {"username":username, "password":hash_value})
+            sql = text("INSERT INTO users (username, password, visible) VALUES (:username, :password, :visible)")
+            self.__db.session.execute(sql, {"username":username, "password":hash_value, "visible":True})
             self.__db.session.commit()
             return
         
     def deactivate_user(self, username) -> Optional[str]:
-        user_id = self.get_entry_id(username)
-        if not user_id:
+        user = self.__get_user(username)
+        if not user:
             return "Username not found in database!"
         else:
             sql = text("UPDATE users SET visible=FALSE WHERE id=:user_id")
-            self.__db.session.execute(sql, {"user_id":user_id})
+            self.__db.session.execute(sql, {"user_id":user.id})
             self.__db.session.commit()
             return
                 
     def login(self, username, password) -> Optional[str]:
-        sql = text("SELECT id, password, visible FROM users WHERE username=:username AND visible=TRUE")
-        result = self.__db.session.execute(sql, {"username":username})
-        user = result.fetchone()
+        user = self.__get_user(username)
         if not user:
             return "Bad username!"
         else:
---
+++ chatidk/psql_db/user.py
@@ -0,0 +1,6 @@
+
+class User:
+    def __init__(self, p_id: int, p_username: str, p_password: str) -> None:
+        self.id = p_id
+        self.username = p_username
+        self.password = p_password

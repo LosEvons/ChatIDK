2e6d68cf72a764eccdbd5ce2fe974e3c41434cfe
Emil Malk <emil.malk@gmail.com>
2023-12-04 10:40:37 +0200

    Removed sensitive information from code

--- chatidk/config.py
+++ chatidk/config.py
@@ -1,13 +1,13 @@
 class Config:
-    SECRET_KEY = "b6375bad9fd2678cd48273b939fbdbb6"
-    SQLALCHEMY_DATABASE_URI = "postgresql:///emilmalk"
+    SECRET_KEY = <your secret key>
+    SQLALCHEMY_DATABASE_URI = <your database uri>
     Debug = False
     UPLOAD_FOLDER = "uploads"
     MAX_CONTENT_PATH = 10000000
     
 class TestConfig:
-    SECRET_KEY = "b6375bad9fd2678cd48273b939fbdbb6"
-    SQLALCHEMY_DATABASE_URI = "postgresql:///emilmalk"
+    SECRET_KEY = <your secret key>
+    SQLALCHEMY_DATABASE_URI = <your database uri>
     TESTING = True
     UPLOAD_FOLDER = "uploads"
     MAX_CONTENT_PATH = 10000000
